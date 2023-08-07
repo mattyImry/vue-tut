707ae6f31dfe24966bf8e89ea23b22a14cd181e4
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-07 14:38:40 +0100

    set types

--- first-vue/src/App.vue
+++ first-vue/src/App.vue
@@ -1,74 +1,73 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue';
+import { ref, watchEffect } from 'vue'
 
+interface WeatherData {
+  name: string
+  main: {
+    temp: number
+  }
+  weather: [
+    {
+      main: string
+    }
+  ]
+}
 
-let query = ref('');
-const weather = ref<any>(null);
- 
+let query = ref('London')
+const weather = ref<WeatherData | null>(null)!
+
+let d: Date = new Date()
+let months: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+let days: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let day: string = days[d.getDay()]
+let date: number = d.getDate()
+let month: string = months[d.getMonth()]
+let year: number = d.getFullYear()
 
 watchEffect(async () => {
   try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${query.value}&units=metric&id=524901&appid=7cc399b56538bbe7b6574e4860bb0802`);
-    const data = await response.json();
-    weather.value = data;
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${query.value}&units=metric&id=524901&appid=7cc399b56538bbe7b6574e4860bb0802`
+    )
+    const data = await response.json()
+    weather.value = data
   } catch (error) {
-    console.error('Error fetching weather data:', error);
+    console.error('Error fetching weather data:', error)
   }
-});
-  
-// export  { 
-//   name: 'app',
-//   data () {
-//     return {
-//       api_key: 'f6a2c317f97367bfe8fa3e5fc517e80d',
-//       url_base: 'https://api.openweathermap.org/data/2.5/',
-//       query: '',
-//       weather: {}
-//     }
-//   },
-//   methods: {
-//     fetchWeather (e) {
-//       if (e.key == "Enter") {
-//         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-//           .then(res => {
-//             return res.json();
-//           }).then(this.setResults);
-//       }
-//     },
-//     setResults (results) {
-//       this.weather = results;
-//     },
-//     dateBuilder () {
-//       let d = new Date();
-//       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//       let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-//       let day = days[d.getDay()];
-//       let date = d.getDate();
-//       let month = months[d.getMonth()];
-//       let year = d.getFullYear();
-
-//       return `${day} ${date} ${month} ${year}`;
-//     }
-//   }
-// }
-
+})
 </script>
 
 <template>
   <main>
-    
-    
     <div class="search-box">
       <form action="">
-      <input type="text" name="search-box" id="search-box" class="search-bar" placeholder="Search" v-model="query" >
-            
-    </form>
+        <input
+          type="text"
+          name="search-box"
+          id="search-box"
+          class="search-bar"
+          placeholder="Search"
+          v-model="query"
+        />
+      </form>
     </div>
-    <div class="weather-wrap" >
+    <div class="weather-wrap">
       <div class="location-box">
         <div class="location">{{ weather.name }}</div>
-        <div class="date">Monday 20</div>
+        <div class="date">{{ day }} {{ date }} {{ month }} {{ year }}</div>
         <div class="weather-box">
           <div class="temp">{{ Math.floor(weather.main.temp) }}C</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
@@ -79,12 +78,12 @@ watchEffect(async () => {
 </template>
 
 <style scoped>
-.search-box{
+.search-box {
   width: 100%;
   margin-bottom: 30px;
 }
 
-.search-box .search-bar{
+.search-box .search-bar {
   display: block;
   width: 100%;
   padding: 15px;
@@ -95,18 +94,18 @@ watchEffect(async () => {
   outline: none;
   background: none;
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255,255,255,0.5);
+  background-color: rgba(255, 255, 255, 0.5);
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
 }
-.search-box .search-bar:focus{
-  background-color: rgba(255,255,255,0.75);
+.search-box .search-bar:focus {
+  background-color: rgba(255, 255, 255, 0.75);
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
-  border-radius:  16px 0px 16px 0px;
+  border-radius: 16px 0px 16px 0px;
 }
 
 .location-box .location {
-  color: #FFF;
+  color: #fff;
   font-size: 32px;
   font-weight: 500;
   text-align: center;
@@ -114,7 +113,7 @@ watchEffect(async () => {
 }
 
 .location-box .date {
-  color: #FFF;
+  color: #fff;
   font-size: 20px;
   font-weight: 300;
   font-style: italic;
@@ -128,12 +127,12 @@ watchEffect(async () => {
 .weather-box .temp {
   display: inline-block;
   padding: 10px 25px;
-  color: #FFF;
+  color: #fff;
   font-size: 102px;
   font-weight: 900;
 
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color:rgba(255, 255, 255, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
   border-radius: 16px;
   margin: 30px 0px;
 
@@ -141,7 +140,7 @@ watchEffect(async () => {
 }
 
 .weather-box .weather {
-  color: #FFF;
+  color: #fff;
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
