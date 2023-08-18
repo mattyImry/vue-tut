cfb965e17fe522f3bc21cf152482fb6c906c55d6
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-18 11:57:30 +0100

    add user input

--- countDown/src/views/Home.vue
+++ countDown/src/views/Home.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="comps">
+    <label for="year">Year:</label>
+    <input type="number" v-model="year" />
+
+    <label for="month">Month:</label>
+    <input type="number" v-model="month" />
+
+    <label for="date">Date:</label>
+    <input type="number" v-model="date" />
+
+    <label for="hour">Hour:</label>
+    <input type="number" v-model="hour" />
+
+    <label for="minute">Minutes:</label>
+    <input type="number" v-model="minute" />
+
+    <label for="second">Seconds:</label>
+    <input type="number" v-model="second" />
+
     <Counter
-      :year="2023"
-      :month="7"
-      :date="18"
-      :hour="11"
-      :minute="46"
-      :second="22"
-      :millisecond="11"
+      :year="year"
+      :month="month"
+      :date="date"
+      :hour="hour"
+      :minute="minute"
+      :second="second"
+      :millisecond="0"
     />
   </div>
 </template>
@@ -16,6 +34,18 @@ import Counter from '@/components/Counter.vue'
 export default {
   components: {
     Counter
+  },
+
+  data() {
+    return {
+      year: 2025,
+      month: 5,
+      date: 22,
+      hour: 10,
+      minute: 55,
+      second: 22,
+      millisecond: 11
+    }
   }
 }
 </script>
