d647093586064154835e269e1bc358b591c76367
Matteo Imeri <matteo.imeri@gmail.com>
2023-08-31 08:24:06 +0100

    updated counter with script setup

--- countDown/src/components/Counter.vue
+++ countDown/src/components/Counter.vue
@@ -10,12 +10,10 @@
       </div>
       <div class="semi">
         {{ displayHours }}
-
         <div class="">Hours</div>
       </div>
       <div class="semi">
         {{ displayMinutes }}
-
         <div class="">Minutes</div>
       </div>
       <div class="semi">
@@ -26,73 +24,63 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: ['year', 'month', 'date', 'hour', 'minute', 'second', 'millisecond'],
-  data: () => ({
-    displayDays: 0,
-    displayHours: 0,
-    displayMinutes: 0,
-    displaySeconds: 0,
-    loaded: false,
-    running: false
-  }),
-  computed: {
-    _seconds: () => 1000,
-    _minutes() {
-      return this._seconds * 60
-    },
-    _hours() {
-      return this._minutes * 60
-    },
-    _days() {
-      return this._hours * 24
-    },
-    end() {
-      return new Date(
-        this.year,
-        this.month,
-        this.date,
-        this.hour,
-        this.minute,
-        this.second,
-        this.millisecond
-      )
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+
+const props = defineProps(['year', 'month', 'date', 'hour', 'minute', 'second', 'millisecond'])
+
+const displayDays = ref(0)
+const displayHours = ref(0)
+const displayMinutes = ref(0)
+const displaySeconds = ref(0)
+const loaded = ref(false)
+const running = ref(false)
+
+const _seconds = 1000
+const _minutes = computed(() => _seconds * 60)
+const _hours = computed(() => _minutes.value * 60)
+const _days = computed(() => _hours.value * 24)
+
+const end = computed(
+  () =>
+    new Date(
+      props.year,
+      props.month,
+      props.date,
+      props.hour,
+      props.minute,
+      props.second,
+      props.millisecond
+    )
+)
+
+const formatNum = (num) => (num < 10 ? '0' + num : num)
+
+const showRemaining = () => {
+  const timer = setInterval(() => {
+    const now = new Date()
+    const distance = end.value.getTime() - now.getTime()
+
+    if (distance < 0) {
+      clearInterval(timer)
+      running.value = true
+      loaded.value = true
+      return
     }
-  },
-  mounted() {
-    this.showRemaining()
-  },
-  methods: {
-    formatNum(num) {
-      return num < 10 ? '0' + num : num
-    },
-    showRemaining() {
-      const timer = setInterval(() => {
-        const now = new Date()
 
-        const distance = this.end.getTime() - now.getTime()
-
-        if (distance < 0) {
-          clearInterval(timer)
-          this.running = true
-          this.loaded = true
-          return
-        }
-
-        const days = Math.floor(distance / this._days)
-        const hours = Math.floor((distance % this._days) / this._hours)
-        const minutes = Math.floor((distance % this._hours) / this._minutes)
-        const seconds = Math.floor((distance % this._minutes) / this._seconds)
-        this.displayMinutes = this.formatNum(minutes)
-        this.displaySeconds = this.formatNum(seconds)
-        this.displayHours = this.formatNum(hours)
-        this.displayDays = this.formatNum(days)
-        this.loaded = true
-      }, 1000)
-    }
-  }
+    const days = Math.floor(distance / _days.value)
+    const hours = Math.floor((distance % _days.value) / _hours.value)
+    const minutes = Math.floor((distance % _hours.value) / _minutes.value)
+    const seconds = Math.floor((distance % _minutes.value) / _seconds)
+    displayMinutes.value = formatNum(minutes)
+    displaySeconds.value = formatNum(seconds)
+    displayHours.value = formatNum(hours)
+    displayDays.value = formatNum(days)
+    loaded.value = true
+  }, 1000)
 }
+
+onMounted(showRemaining)
 </script>
 
 <style lang="css" scoped>
