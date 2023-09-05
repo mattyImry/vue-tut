71f2497a94189fb6b88db9f122b6ff3643da0256
matty <matteo.imeri@gmail.com>
2023-09-05 10:09:44 +0100

    contDownTs prj start

---
+++ countDownTS/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Home from './views/Home.vue'
+</script>
+
+<template>
+  <div class="app">
+    <Home />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
